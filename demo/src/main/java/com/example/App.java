5745dd8b88036fc0b94a8aa155f047ebc27bcd00
Le Huong <Dongnguyen210300@gmail.com>
2024-12-06 16:02:02 +0700

    sua cau truc thu muc

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * JavaFX App
@@ -17,7 +18,10 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("com/example/picture/home_picture.png");
+
+
+        scene = new Scene(loadFXML("primary"), 1000, 600);
         stage.setScene(scene);
         stage.show();
     }
