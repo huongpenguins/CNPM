82416ad7897bbe7ab2f8c5258ebaa703ea02829d
tranducthu2003 <thu.td225412@sis.hust.edu.vn>
2024-12-11 16:01:28 +0700

    connect database

---
+++ demo/src/main/java/Entities/HoGiaDinh.java
@@ -0,0 +1,14 @@
+package Entities;
+
+import java.time.LocalDate;
+
+public class HoGiaDinh {
+    private String MaHoGiaDinh;
+    private int MaCanHo;
+    private int MaNhanKhau;
+    private int MaXe;
+    private LocalDate NgayCap;
+    private String TenChuHo;
+    private int SDT;
+
+}
---
+++ demo/src/main/java/Entities/HoaDon.java
@@ -0,0 +1,12 @@
+package Entities;
+
+public class HoaDon {
+    private int MaHoaDon;
+    private int MaKhoanThuTheoHo;
+    private String TenHoaDon;
+    private int SoTienDaNop;
+
+    public static void main(String[] args) {
+
+    }
+}
