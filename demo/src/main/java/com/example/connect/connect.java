8e9b17501d0b41962bd836c77c069baa05747966
dangphank4 <phanlehaidanghsht@gmail.com>
2024-12-13 04:04:18 +0700

    sql_sever

--- demo/src/main/java/com/example/connect/connect.java
+++ demo/src/main/java/com/example/connect/connect.java
@@ -6,13 +6,15 @@ import java.sql.SQLException;
 
 public class connect {
     public static void main(String[] args) throws SQLException {
-        var url = "jdbc:mysql://localhost:3306/quanlychungcu";
-        var user = "root";
-        var password = "2003";
-        try(Connection conn = DriverManager.getConnection(url,user,password);){
+        var url1 = "jdbc:mysql://localhost:3306/quanlychungcu";
+        var user1 = "root";
+        var password1 = "2003";
+        try(Connection conn = DriverManager.getConnection(url1,user1,password1);){
             System.out.println("connect the database");
         }catch (SQLException e){
             System.err.println("connect failed: "+ e.getMessage());
         }
+
+
     }
 }
--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -2,7 +2,8 @@ module com.example {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-
+    requires com.microsoft.sqlserver.jdbc;
+    requires java.naming;
 
     opens com.example to javafx.fxml;
     exports com.example;
