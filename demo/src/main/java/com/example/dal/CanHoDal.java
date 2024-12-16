0b255137d105c88c41f5f95eb5c89b1d55b882bb
Le Huong <Dongnguyen210300@gmail.com>
2024-12-17 01:10:43 +0700

    .

--- demo/src/main/java/com/example/dal/CanHoDal.java
+++ demo/src/main/java/com/example/dal/CanHoDal.java
@@ -1,6 +1,9 @@
 package com.example.dal;
 import com.example.Admin;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class CanHoDAL extends Admin {
 
     public CanHoDAL() {
@@ -11,11 +14,18 @@ public class CanHoDAL extends Admin {
      * Hàm thêm dữ liệu vào bảng CanHo
      * @return true nếu thêm thành công, false nếu thất bại
      */
+    static String tableName = "canhotbl";
     public boolean insertCanHo() {
-        String tableName = "canhotbl";
         String[] columns = {"MaCanHo","MaHoKhau","TenCanHo","Tang","DienTich","MoTa"};
         String[] types = {"int","String","String","int","float","String"};
 
         return insert(tableName, columns, types); // Gọi hàm insert từ Admin
+    }
+    public static boolean updateCanHo(String tableName, String columnName, String newValue, String conditionColumn, String conditionValue) throws SQLException {
+
+        return update(tableName, columnName, newValue, conditionColumn,conditionValue);
+    }
+    public static boolean deleteCanHo(String tableName,String columnName,String columnValue){
+        return delete(tableName,columnName,columnValue);
     }
 }
