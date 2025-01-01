345957b4fe72e871d1d73030fa3725e0f77c8577
phongbeovaio <lenamphongsoict@gmail.com>
2025-01-02 03:51:31 +0700

    Sửa lại HoGiaDinh và NhanKhau cho đúng với Data mới!

--- demo/src/main/java/com/example/Household.java
+++ demo/src/main/java/com/example/Household.java
@@ -6,17 +6,17 @@ public class Household {
     private String residentId;
   //  private String vehicleId;
     private String issueDate;
-    private String ownerName;
-    private String phoneNumber;
+  //  private String ownerName;
+   // private String phoneNumber;
 
-    public Household(String householdId, String apartmentId, String residentId /*String vehicleId*/, String issueDate, String ownerName, String phoneNumber) {
+    public Household(String householdId, String apartmentId, String residentId /*String vehicleId*/, String issueDate /* String ownerName */ /*String phoneNumber*/) {
         this.householdId = householdId;
         this.apartmentId = apartmentId;
         this.residentId = residentId;
      //   this.vehicleId = vehicleId;
         this.issueDate = issueDate;
-        this.ownerName = ownerName;
-        this.phoneNumber = phoneNumber;
+      //  this.ownerName = ownerName;
+       // this.phoneNumber = phoneNumber;
     }
 
     public String getHouseholdId() {
@@ -40,11 +40,14 @@ public class Household {
         return issueDate;
     }
 
+    /*
     public String getOwnerName() {
         return ownerName;
     }
-
+    */
+    /*
     public String getPhoneNumber() {
         return phoneNumber;
     }
+     */
 }
